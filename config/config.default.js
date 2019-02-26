658b2432d56dcd9b624effcf8ccbada6484b5abd
妙蛙种子 <zjl198544@163.com>
2019-02-26 13:10:52 +0800

    first commit

---
+++ config/config.default.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/**
+ * egg-influxdb-plugin default config
+ * @member Config#influxdbPlugin
+ * @property {String} SOME_KEY - some description
+ */
+exports.influxdbPlugin = {
+
+};
